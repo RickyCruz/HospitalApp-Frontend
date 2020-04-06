ff763e07c04963a876ebbbe1ed051ff98a235b5e
Ricky Cruz <rczdev@gmail.com>
2020-04-06 14:21:30 -0500

    Define project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { GraphicsOneComponent } from './pages/graphics-one/graphics-one.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcumbsComponent } from './shared/breadcumbs/breadcumbs.component';
+import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    GraphicsOneComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcumbsComponent,
+    NoPageFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/graphics-one/graphics-one.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphics-one',
+  templateUrl: './graphics-one.component.html',
+  styles: []
+})
+export class GraphicsOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
