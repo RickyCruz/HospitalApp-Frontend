7d5a9ae8eeac464ea6fc78603e3f33b016901a56
Ricky Cruz <rczdev@gmail.com>
2020-04-13 13:46:30 -0500

    Control sidebar with a service

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
 
 export class SidebarService {
+  menu: any = [
+    {
+      title: 'General',
+      icon: 'mdi mdi-gauge',
+      submenu: [
+        { title: 'Dashboard', url: '/dashboard' },
+        { title: 'ProgressBar', url: '/progress' },
+        { title: 'Charts', url: '/charts-one' },
+        { title: 'Settings', url: '/account-settings' },
+      ]
+    },
+  ];
 
   constructor() { }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 import { HeaderComponent } from './header/header.component';
@@ -6,6 +8,10 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { BreadcumbsComponent } from './breadcumbs/breadcumbs.component';
 
 @NgModule({
+  imports:[
+    RouterModule,
+    CommonModule
+  ],
   declarations: [
     HeaderComponent,
     SidebarComponent,
